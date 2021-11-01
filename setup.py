8b5fd046bd22253e8b03da831d4a07cf9da28644
Alright Yagami <77753224+LAUGHING-CAT-MEME@users.noreply.github.com>
2021-11-01 10:54:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
-from setuptools import setup
-try:
-    import pypandoc
-    description = pypandoc.convert('README.md','rst')
-    
-except:
-    description=''
-setup(name='PyLyrics',
-      version="1.1.0",
-      description='Pythonic Implementation of lyrics.wikia.com',
-      long_description=description,
-      author="Pradipta Bora",
-      author_email='pradd@outlook.com',
-      license='MIT',
-      packages=['PyLyrics'],
-      url="http://github.com/geekpradd/PyLyrics",
-      install_requires=[
-            'beautifulsoup4','requests',],
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Topic :: Internet",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python"
-      ],
-      zip_safe=False)
+bstr = ""
+coro = {
+    "1": "001",
+    "2": "010",
+    "3": "011",
+    "4": "100",
+    "5": "101",
+    "6": "110",
+    "7": "111",
+    "8": "0011",
+    "9": "1100",
+    "0": "000",
+    " ": "",
+    ".": ""
+}
+while True:
+    inp = input ("8=D  ")
+    for n in inp:
+        bstr += coro[n]
+    print (f"{bin(int(bstr))}")
